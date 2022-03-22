ac0b01c2cf2c33aef45f152823c4c1a9a8532dde
Nick Elslager <nickelslager@gmail.com>
2022-03-22 11:28:02 -0400

    Redux Actions

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -8,6 +8,7 @@ import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
+import { addComment } from "../redux/ActionCreators";
 
 //No longer need with redux
 //import { CAMPSITES } from "../shared/campsites";
@@ -22,6 +23,11 @@ const mapStateToProps = (state) => {
     partners: state.partners,
     promotions: state.promotions,
   };
+};
+
+const mapDispatchToProps = {
+  addComment: (campsiteId, rating, author, text) =>
+    addComment(campsiteId, rating, author, text),
 };
 class Main extends Component {
   //NO LONGER NEED
@@ -60,6 +66,7 @@ class Main extends Component {
           comments={this.props.comments.filter(
             (comment) => comment.campsiteId === +match.params.campsiteId
           )}
+          addComment={this.props.addComment}
         />
       );
     };
@@ -87,4 +94,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
